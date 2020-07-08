f33c0c14060afd02bc19a55477dfbb03b7ad0bd4
robotmaterkris <kris@robotwealth.com>
2020-07-08 20:43:18 +1000

    add kiwi data, swap out cad for kiwi in heatmaps

--- R/globals.R
+++ R/globals.R
@@ -4,13 +4,13 @@ theme_set(theme_bw())
 timezone_list <- c("USD in ET" = "USD", "EUR in CET" = "EUR", "JPY in JST" = "JPY")
 timezone_map <- c("USD" = "ET", "EUR" = "CET", "JPY" = "JST")
 assets_list <- list(
-  "USD" = c("AUDUSD", "CADUSD", "CHFUSD", "EURUSD", "GBPUSD", "JPYUSD"),
-  "EUR" = c("AUDEUR", "CADEUR", "CHFEUR", "GBPEUR", "JPYEUR", "USDEUR"),
-  "JPY" = c("AUDJPY", "CADJPY", "CHFJPY", "EURJPY", "GBPJPY", "USDJPY")
+  "USD" = c("AUDUSD", "NZDUSD", "CHFUSD", "EURUSD", "GBPUSD", "JPYUSD", "CADUSD"),
+  "EUR" = c("AUDEUR", "NZDEUR", "CHFEUR", "GBPEUR", "JPYEUR", "USDEUR", "CADEUR"),
+  "JPY" = c("AUDJPY", "NZDJPY", "CHFJPY", "EURJPY", "GBPJPY", "USDJPY", "CADJPY")
 )
 hm_assets_list <- list(
-  "USD" = c("AUDUSD", "USDCAD", "USDCHF", "EURUSD", "GBPUSD", "USDJPY"),
-  "EUR" = c("EURAUD", "EURCAD", "EURCHF", "EURGBP", "EURJPY", "EURUSD"),
-  "JPY" = c("AUDJPY", "CADJPY", "CHFJPY", "EURJPY", "GBPJPY", "USDJPY")
+  "USD" = c("AUDUSD", "NZDUSD", "USDCHF", "EURUSD", "GBPUSD", "USDJPY"),
+  "EUR" = c("EURAUD", "EURNZD", "EURCHF", "EURGBP", "EURJPY", "EURUSD"),
+  "JPY" = c("AUDJPY", "NZDJPY", "CHFJPY", "EURJPY", "GBPJPY", "USDJPY")
 )
 hm_date_ranges <- c("2009-2011", "2012-2014", "2015-2017", "2018-2020", "2009-2020")
--- data/make_returns.data.R
+++ data/make_returns.data.R
@@ -1,15 +1,24 @@
 library(tidyverse)
 # Calculate Returns and Convert to Eastern Time (this is now loaded on startup)
+
+DATA_FOLDER <- 'C:/Users/Kris/ResilioSync/FXBootcamp'
+source(paste0(DATA_FOLDER,'/Code/tools/r-tools/data-utils.R'))
+
+asset_list <- get_asset_list("AssetsDWX-FX-USD")
+raw_prices_df <- get_hourly_OHLC("NZDJPY")
+usd_prices_df <- convert_common_quote_currency(raw_prices_df, quote_currency = 'JPY')
+assetNames <- distinct(usd_prices_df, Ticker)
+
 usd_returns <-  usd_prices_df %>%
   group_by(Ticker) %>%
   mutate(returns = Close / lag(Close) - 1)
 
 datetime <- as.POSIXct(paste(usd_returns$Date, usd_returns$Time), format="%Y-%m-%d %H:%M", tz='UTC')
-attributes(datetime)$tzone <- 'America/New_York'
+attributes(datetime)$tzone <- 'Japan'  # 'America/New_York'
 usd_returns$datetime <- datetime
 
-returns_df <- usd_returns %>%
-  filter(Ticker %in% c("AUDUSD", "CADUSD", "CHFUSD", "EURUSD", "GBPUSD", "JPYUSD")) %>%
+df <- usd_returns %>%
+  filter(Ticker %in% c("NZDJPY")) %>%
   mutate(hour = hour(datetime),
          year = year(datetime),
          month = month(datetime),
@@ -17,4 +26,10 @@ returns_df <- usd_returns %>%
   select(Ticker, datetime, hour, year, month, day, returns) %>%
   na.omit()
 
-save(returns_df, file = "usd_returns_df.RData")
+returns_df <- bind_rows(list(returns_df, df))
+df <- returns_df
+load(here::here("data", "returns_df.RData"))
+
+returns_df <- returns_df %>% arrange(datetime)
+
+save(returns_df, file = here::here("data", "returns_df.RData"))
